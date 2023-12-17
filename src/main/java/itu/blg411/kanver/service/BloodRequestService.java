e65728ac5138f8a9d708aef27cbc284afac4064b
Mawada Abdelgayed <abdelgayed20@itu.edu.tr>
2023-12-17 15:59:00 +0300

    ...

--- src/main/java/itu/blg411/kanver/service/BloodRequestService.java
+++ src/main/java/itu/blg411/kanver/service/BloodRequestService.java
@@ -1,19 +1,16 @@
-package itu.blg411.kanver.service;
-
-import itu.blg411.kanver.dto.BloodRequestDto;
-
-import java.util.List;
-
-public interface BloodRequestService {
-    BloodRequestDto findById(Long id);
-
-    List<BloodRequestDto> findAll();
-
-    BloodRequestDto save(BloodRequestDto bloodRequestDto);
-
-    void deleteById(Long id);
-
-    //PageSearchResult<BloodRequestDto> search(BloodRequestSearchCriteria criteria);
-
-
-}
+//package itu.blg411.kanver.service;
+//
+//import itu.blg411.kanver.dto.BloodRequestDto;
+//
+//import java.util.List;
+//
+//public interface BloodRequestService {
+//    BloodRequestDto findById(Long id);
+//
+//    List<BloodRequestDto> findAll();
+//
+//    BloodRequestDto save(BloodRequestDto bloodRequestDto);
+//
+//    void deleteById(Long id);
+//
+//}

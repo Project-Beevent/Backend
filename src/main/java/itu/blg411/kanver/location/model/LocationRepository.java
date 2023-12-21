143eb636e3f1cd4f88a0e6c1691ff50f718d01d0
Mawada Abdelgayed <abdelgayed20@itu.edu.tr>
2023-12-21 06:08:21 +0300

    docs: Optimize Structure and Document API Endpoints in Location Module

--- src/main/java/itu/blg411/kanver/location/model/LocationRepository.java
+++ src/main/java/itu/blg411/kanver/location/model/LocationRepository.java
@@ -4,6 +4,9 @@ import itu.blg411.kanver.location.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    List<Location> findByCity(String cityName);
 }

9cc324be909cb274d60b40f291a8eb5a6cc4e4ab
Mawada Abdelgayed <abdelgayed20@itu.edu.tr>
2023-12-22 06:07:31 +0300

    feat: Added Swagger for API documentation

--- src/main/java/itu/blg411/kanver/KanverApplication.java
+++ src/main/java/itu/blg411/kanver/KanverApplication.java
@@ -2,7 +2,6 @@ package itu.blg411.kanver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 public class KanverApplication {

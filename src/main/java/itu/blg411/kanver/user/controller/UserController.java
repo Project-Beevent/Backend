0638f01b0d540ab1097c9b6d98fb38d09048567e
Mawada Abdelgayed <abdelgayed20@itu.edu.tr>
2023-12-31 15:01:21 +0300

    fix: Cors issue fixed

--- src/main/java/itu/blg411/kanver/user/controller/UserController.java
+++ src/main/java/itu/blg411/kanver/user/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {

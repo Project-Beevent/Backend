74765fd86d492c3155ef72ce30db30382746f954
Mawada Abdelgayed <abdelgayed20@itu.edu.tr>
2023-12-31 12:41:24 +0300

    Updated CorsConfig

--- src/main/java/itu/blg411/kanver/config/CorsConfig.java
+++ src/main/java/itu/blg411/kanver/config/CorsConfig.java
@@ -1,23 +1,13 @@
 package itu.blg411.kanver.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.addAllowedOrigin("*");
-
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
